ca0b8c8047876684a72f549f3bfbc476b721d00d
AgentKazy <kazygaming@outlook.pt>
2023-03-11 17:41:37 +0000

    Destructuring (Continued)

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+////////////////////////////////////////////////
 // Destructuring
 
 const person = {
@@ -6,5 +7,42 @@ const person = {
   favColor: 'Blue',
 };
 
-const firstName = person.firstName;
-console.log(firstName);
+// Usual way
+// const firstName = person.firstName;
+
+// Destructure
+const { firstName, favColor, age } = person;
+
+console.log(firstName, favColor, age);
+
+////////////////////////////////////////////////
+// Array destructuring
+const nums = [7, 9, 3, 5, 2];
+
+const [a, b, c] = nums;
+
+console.log(a, b, c);
+
+////////////////////////////////////////////////
+// Functions
+
+const getCalculations = (a, b) => {
+  // return {
+  //   sum: a + b,
+  //   diff: Math.abs(a - b),
+  //   product: a * b,
+  // };
+
+  return [a + b, Math.abs(a - b), a * b];
+};
+
+const [sum, diff, product] = getCalculations(7, 9);
+console.log(sum, diff, product);
+
+////////////////////////////////////////////////
+// Props
+const doSomething = ({ name }) => {
+  console.log(name);
+};
+
+doSomething({ name: 'Bruno', age: 29 });
