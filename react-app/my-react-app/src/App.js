fe9915e427e1df5e0329a9a7ce29fecd3bc953f7
AgentKazy <kazygaming@outlook.pt>
2023-03-15 12:27:03 +0000

    21. Using the Previous State

--- react-app/my-react-app/src/App.js
+++ react-app/my-react-app/src/App.js
@@ -2,27 +2,29 @@ import './App.css';
 import { useState } from 'react';
 
 function App() {
-  const [name, setName] = useState('Kazy');
   const [events, setEvents] = useState([
     { title: 'Adventure to Mount Doom', id: 1 },
     { title: 'Escaping Moria', id: 2 },
     { title: 'Resting in Rivendell', id: 3 },
   ]);
 
-  const handleClick = () => {
-    setName('Kalethyr');
-    console.log(name);
+  const handleClick = (id) => {
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => {
+        return id !== event.id;
+      });
+    });
+    console.log(id);
   };
 
   return (
     <div className="App">
-      <h1>My name is {name}</h1>
-      <button onClick={handleClick}>Change name</button>
       {events.map((event, index) => (
         <div key={event.id}>
           <h2>
             {index} - {event.title}
           </h2>
+          <button onClick={() => handleClick(event.id)}>Delete event</button>
         </div>
       ))}
     </div>
