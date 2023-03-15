930cdc2e2e34b5a25d3444d6345d00b7504f34c2
AgentKazy <kazygaming@outlook.pt>
2023-03-15 12:14:08 +0000

    20. Outputting Lists

--- react-app/my-react-app/src/App.js
+++ react-app/my-react-app/src/App.js
@@ -3,6 +3,11 @@ import { useState } from 'react';
 
 function App() {
   const [name, setName] = useState('Kazy');
+  const [events, setEvents] = useState([
+    { title: 'Adventure to Mount Doom', id: 1 },
+    { title: 'Escaping Moria', id: 2 },
+    { title: 'Resting in Rivendell', id: 3 },
+  ]);
 
   const handleClick = () => {
     setName('Kalethyr');
@@ -13,6 +18,13 @@ function App() {
     <div className="App">
       <h1>My name is {name}</h1>
       <button onClick={handleClick}>Change name</button>
+      {events.map((event, index) => (
+        <div key={event.id}>
+          <h2>
+            {index} - {event.title}
+          </h2>
+        </div>
+      ))}
     </div>
   );
 }
