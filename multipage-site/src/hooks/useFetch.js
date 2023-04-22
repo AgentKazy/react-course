59299099f4580cf9bda1d4e9684fccfc134e4288
AgentKazy <kazygaming@outlook.pt>
2023-04-22 13:08:06 +0100

    73. Fetching Data

---
+++ multipage-site/src/hooks/useFetch.js
@@ -0,0 +1,45 @@
+import { useState, useEffect, useRef } from 'react';
+
+export const useFetch = (url, _options) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(false);
+  const [error, setError] = useState(null);
+
+  // Use useRef to wrap an obect/array argument
+  // which is a useEffect dependency
+  const options = useRef(_options).current;
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const fetchData = async () => {
+      setIsPending(true);
+
+      try {
+        const res = await fetch(url, { signal: controller.signal });
+        if (!res.ok) {
+          throw new Error(res.statusText);
+        }
+        const json = await res.json();
+
+        setIsPending(false);
+        setData(json);
+        setError(null);
+      } catch (err) {
+        if (err.name === 'AbortError') {
+          console.log('The fecth was aborted.');
+        } else {
+          setIsPending(false);
+          setError('Could not fetch the data');
+        }
+      }
+    };
+    fetchData();
+
+    return () => {
+      controller.abort();
+    };
+  }, [url]);
+
+  return { data, isPending, error };
+};
