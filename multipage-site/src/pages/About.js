a9c4095db9be10a1c8bb01a570c3197d34018585
AgentKazy <kazygaming@outlook.pt>
2023-04-22 11:02:58 +0100

    70. React Router Setup

---
+++ multipage-site/src/pages/About.js
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
+        laudantium assumenda rerum velit nam autem nostrum et maxime
+        voluptatibus a, quisquam ipsa obcaecati eveniet quasi, tenetur officiis,
+        consequuntur magni cum?
+      </p>
+    </div>
+  );
+}
---
+++ multipage-site/src/pages/Contact.js
@@ -0,0 +1,13 @@
+export default function Contact() {
+  return (
+    <div>
+      <h2>Contact</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odit
+        laudantium assumenda rerum velit nam autem nostrum et maxime
+        voluptatibus a, quisquam ipsa obcaecati eveniet quasi, tenetur officiis,
+        consequuntur magni cum?
+      </p>
+    </div>
+  );
+}
