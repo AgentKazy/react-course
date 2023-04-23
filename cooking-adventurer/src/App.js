99355570b753691efb1cfcc73b14d08c3a6eba1b
AgentKazy <kazygaming@outlook.pt>
2023-04-23 21:33:58 +0100

    81. Making a Navbar Component

--- cooking-adventurer/src/App.js
+++ cooking-adventurer/src/App.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom/cjs/react-router-dom.min';
 
 // Page components
+import Navbar from './components/Navbar';
 import Home from './pages/home/Home';
 import Create from './pages/create/Create';
 import Search from './pages/search/Search';
@@ -17,19 +18,17 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path="/">
             <Home />
           </Route>
-
           <Route path="/create">
             <Create />
           </Route>
-
           <Route path="/search">
             <Search />
           </Route>
-
           <Route path="/recipes/:id">
             <Recipe />
           </Route>
