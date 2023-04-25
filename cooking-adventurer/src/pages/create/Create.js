26070ef28bd1c314166f3e21265649a0968f2302
AgentKazy <kazygaming@outlook.pt>
2023-04-25 13:18:07 +0100

    89. CHALLENGE - Redirecting the User

--- cooking-adventurer/src/pages/create/Create.js
+++ cooking-adventurer/src/pages/create/Create.js
@@ -1,4 +1,5 @@
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import { useFetch } from '../../hooks/useFetch';
 
 // Styles
@@ -11,6 +12,7 @@ export default function Create() {
   const [newIngredient, setNewIngredient] = useState('');
   const [ingredients, setIngredients] = useState([]);
   const ingredientInput = useRef(null);
+  const history = useHistory();
 
   const { postData, data, error } = useFetch(
     'http://localhost:3000/recipes',
@@ -37,6 +39,13 @@ export default function Create() {
     setNewIngredient('');
     ingredientInput.current.focus();
   };
+
+  // Redirect user when we get data response
+  useEffect(() => {
+    if (data) {
+      history.push('/');
+    }
+  }, [data]);
 
   return (
     <div className="create">
