1a4e2797dbc7600dc87d7140bbfc729f2594316b
AgentKazy <kazygaming@outlook.pt>
2023-04-29 18:51:40 +0100

    114. Creating a Navbar

--- simple-finance/src/App.js
+++ simple-finance/src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+// Pages & components
 import Home from './pages/home/Home';
 import Login from './pages/login/Login';
 import Signup from './pages/signup/Signup';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Switch>
           <Route exact path="/">
             <Home />
