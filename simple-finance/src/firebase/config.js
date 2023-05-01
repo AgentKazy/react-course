4a88ee7c292d62f8106e01563aa37813cc4a2654
AgentKazy <kazygaming@outlook.pt>
2023-05-01 12:29:59 +0100

    117. Firebase Setup

---
+++ simple-finance/src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDRHl0zntGtx65T9xDKYLk5D3sQoLFwNcU',
+  authDomain: 'kazy-finance.firebaseapp.com',
+  projectId: 'kazy-finance',
+  storageBucket: 'kazy-finance.appspot.com',
+  messagingSenderId: '1091610249963',
+  appId: '1:1091610249963:web:61b1171eb32da829d34cae',
+};
+
+// Init Firebase
+firebase.initializeApp(firebaseconfig);
+
+// Init Services
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
