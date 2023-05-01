2dcc638215b6192d08a6c3a50e0a299020f7c511
AgentKazy <kazygaming@outlook.pt>
2023-05-01 16:57:32 +0100

    124. Using the Logout Hook

--- simple-finance/src/components/Navbar.js
+++ simple-finance/src/components/Navbar.js
@@ -1,9 +1,11 @@
 import { Link } from 'react-router-dom';
-
+import { useLogout } from '../hooks/useLogout';
 // Styles
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
+  const { logout } = useLogout();
+
   return (
     <nav className={styles.navbar}>
       <ul>
@@ -12,6 +14,12 @@ export default function Navbar() {
         <li>
           <Link to="/login">Login</Link>
           <Link to="/signup">Signup</Link>
+        </li>
+
+        <li>
+          <button className="btn" onClick={logout}>
+            Logout
+          </button>
         </li>
       </ul>
     </nav>
