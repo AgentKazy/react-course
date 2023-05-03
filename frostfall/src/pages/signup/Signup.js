39a2f180a90f83c92aeec800a0697017c5e3b9a3
AgentKazy <kazygaming@outlook.pt>
2023-05-03 17:35:34 +0100

    157. Creating a Signup Page

--- frostfall/src/pages/signup/Signup.js
+++ frostfall/src/pages/signup/Signup.js
@@ -1,10 +1,55 @@
+import { useState } from 'react';
+
 // Styles
 import './Signup.css';
 
 export default function Signup() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('');
+  const [thumbnail, setThumbnail] = useState(null);
+
   return (
-    <div>
-      <h1>Signup</h1>
-    </div>
+    <form className="auth-form">
+      <h2>Sign Up</h2>
+      <label>
+        {/* <span>Email:</span> */}
+        <input
+          required
+          type="email"
+          placeholder="Email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+
+      <label>
+        {/* <span>Password:</span> */}
+        <input
+          required
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+
+      <label>
+        {/* <span>Display name:</span> */}
+        <input
+          required
+          type="text"
+          placeholder="Display name"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+
+      <label>
+        <span>Profile picture:</span>
+        <input required type="file" />
+      </label>
+      <button className="btn">Sign Up</button>
+    </form>
   );
 }
