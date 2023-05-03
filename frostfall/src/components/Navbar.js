f48837b229a2bf0d92f57ae75a69908b2afef20e
AgentKazy <kazygaming@outlook.pt>
2023-05-03 16:23:17 +0100

    155. Navbar Component

---
+++ frostfall/src/components/Navbar.js
@@ -0,0 +1,27 @@
+import { Link } from 'react-router-dom';
+
+// Styles & Images
+import './Navbar.css';
+import Snowflake from '../assets/snowflake-light.svg';
+
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <ul>
+        <li className="logo">
+          <img src={Snowflake} alt="Frostfall logo of a snowflake" />
+          <span>Frostfall</span>
+        </li>
+
+        <li>
+          <Link to="/login">Login</Link>
+          <Link to="/signup">Signup</Link>
+        </li>
+
+        <li>
+          <button className="btn">Logout</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
