234d1a212e5b7c3594edd61c99015633948c7893
AgentKazy <kazygaming@outlook.pt>
2023-05-03 14:34:54 +0100

    151. Firebase Setup

---
+++ frostfall/src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDMNlizaG4S91RGvraEI6M5pVCyX2Z4u5E',
+  authDomain: 'frostfall-web.firebaseapp.com',
+  projectId: 'frostfall-web',
+  storageBucket: 'frostfall-web.appspot.com',
+  messagingSenderId: '671911324267',
+  appId: '1:671911324267:web:709abdc060c2a177ba9f2e',
+};
+
+// Init Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Init Services
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// Timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
