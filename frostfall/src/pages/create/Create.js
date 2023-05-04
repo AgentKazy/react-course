8115672c20b5938e9b3a46899549e51ed9e9c12d
AgentKazy <kazygaming@outlook.pt>
2023-05-04 12:55:54 +0100

    171. Making the "Create Project" Form

--- frostfall/src/pages/create/Create.js
+++ frostfall/src/pages/create/Create.js
@@ -1,10 +1,69 @@
+import { useState } from 'react';
+
 // Styles
 import './Create.css';
 
 export default function Create() {
+  // Form field values
+  const [name, setName] = useState('');
+  const [details, setDetails] = useState('');
+  const [dueDate, setDueDate] = useState('');
+  const [category, setCategory] = useState('');
+  const [assignedUsers, setAssignedUsers] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, details, dueDate);
+  };
+
   return (
-    <div>
-      <h1>Create</h1>
+    <div className="create-form">
+      <h2 className="page-title">Create a new project</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          {/* <span>Project name</span> */}
+          <input
+            required
+            type="text"
+            placeholder="Project name"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+        </label>
+
+        <label>
+          {/* <span>Project details</span> */}
+          <textarea
+            required
+            type="text"
+            placeholder="Details"
+            onChange={(e) => setDetails(e.target.value)}
+            value={details}
+          ></textarea>
+        </label>
+
+        <label>
+          <span>Set due date:</span>
+          <input
+            required
+            type="date"
+            onChange={(e) => setDueDate(e.target.value)}
+            value={dueDate}
+          />
+        </label>
+
+        <label>
+          <span>Project category</span>
+          {/* select category here */}
+        </label>
+
+        <label>
+          <span>Assigned to</span>
+          {/* Assignees here */}
+        </label>
+
+        <button className="btn">Add Project</button>
+      </form>
     </div>
   );
 }
